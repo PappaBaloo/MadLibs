cc35ce044dfe3ea10b35fabd1f9f7f284799dc9a
Charlie Nylund <thebettercharlie@gmail.com>
2020-10-04 22:25:30 +0200

    firstcommit

---
+++ MadLibs010101/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MadLibs010101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string LargeRegion;
+            string CountryInRegion;
+            string PositionOfPower;
+            string Celebrity;
+            string CaoCaoChina;
+
+            Console.Write("Enter a large region of the world: ");
+            LargeRegion = Console.ReadLine();
+
+            Console.Write("Enter a country in that region: ");
+            CountryInRegion = Console.ReadLine();
+
+            Console.Write("Enter a position of power: ");
+            PositionOfPower = Console.ReadLine();
+
+            Console.Write("Enter a celebrity name: ");
+            Celebrity = Console.ReadLine();
+
+            CaoCaoChina = "During the turmoil of " + LargeRegion + " , " + Celebrity + " was able to secure most of " + CountryInRegion + " - the most populated and developed part of " + CountryInRegion + ". " + Celebrity + " was also very successful in restoring public order and rebuilding the economy as the " + PositionOfPower + "";
+            Console.WriteLine(CaoCaoChina);
+
+            Console.ReadKey();
+        }
+    }
+}
